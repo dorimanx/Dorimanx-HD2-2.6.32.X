db3e0822ee5832ea7ab57d0a2c3a63ea3057e66b
Dorimanx <yuri@bynet.co.il>
2012-03-17 01:46:06 +0200

    Reverted Q6 change (ACA)

--- arch/arm/mach-msm/qdsp6_1550/audio_ctl.c
+++ arch/arm/mach-msm/qdsp6_1550/audio_ctl.c
@@ -133,7 +133,8 @@ static int q6_open(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static long q6_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+static int q6_ioctl(struct inode *inode, struct file *file,
+	unsigned int cmd, unsigned long arg)
 {
 	int rc;
 	uint32_t n;
@@ -248,7 +249,7 @@ static int q6_release(struct inode *inode, struct file *file)
 static struct file_operations q6_dev_fops = {
 	.owner		= THIS_MODULE,
 	.open		= q6_open,
-	.unlocked_ioctl    = q6_ioctl,
+	.ioctl    	= q6_ioctl,
 	.release	= q6_release,
 };
 
