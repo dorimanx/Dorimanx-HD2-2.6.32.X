b5d6fe9fd8e12d7c42d0fb00c3e86472af1db028
Dorimanx <yuri@bynet.co.il>
2012-03-17 03:05:50 +0200

    firmware loader: allow builtin firmware load even if usermodehelper is disabled author	Linus Torvalds <torvalds@linux-foundation.org> 	Wed, 29 Feb 2012 06:53:20 +0000 (11:53 +0530) committer	Greg Kroah-Hartman <gregkh@linuxfoundation.org> 	Sun, 4 Mar 2012 17:49:23 +0000 (09:49 -0800) [ Upstream commit caca9510ff4e5d842c0589110243d60927836222 ]
    In commit a144c6a6c924 ("PM: Print a warning if firmware is requested
    when tasks are frozen") we not only printed a warning if somebody tried
    to load the firmware when tasks are frozen - we also failed the load.
    
    But that check was done before the check for built-in firmware, and then
    when we disallowed usermode helpers during bootup (commit 288d5abec831:
    "Boot up with usermodehelper disabled"), that actually means that
    built-in modules can no longer load their firmware even if the firmware
    is built in too.  Which used to work, and some people depended on it for
    the R100 driver.
    
    So move the test for usermodehelper_is_disabled() down, to after
    checking the built-in firmware.
    
    This should fix:
    
    https://bugzilla.kernel.org/show_bug.cgi?id=40952
    
    Reported-by: James Cloos <cloos@hjcloos.com>
    Bisected-by: Elimar Riesebieter <riesebie@lxtec.de>
    Cc: Michel D.nzer <michel@daenzer.net>
    Cc: Rafael Wysocki <rjw@sisk.pl>
    Cc: Valdis Kletnieks <valdis.kletnieks@vt.edu>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Srivatsa S. Bhat <srivatsa.bhat@linux.vnet.ibm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -489,11 +489,6 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 	if (!firmware_p)
 		return -EINVAL;
 
-        if (WARN_ON(usermodehelper_is_disabled())) {
-                dev_err(device, "firmware: %s will not be loaded\n", name);
-                return -EBUSY;
-        }
-
 	*firmware_p = firmware = kzalloc(sizeof(*firmware), GFP_KERNEL);
 	if (!firmware) {
 		dev_err(device, "%s: kmalloc(struct firmware) failed\n",
@@ -512,6 +507,12 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 		firmware->data = builtin->data;
 		return 0;
 	}
+
+        if (WARN_ON(usermodehelper_is_disabled())) {
+                dev_err(device, "firmware: %s will not be loaded\n", name);
+                retval = -EBUSY;
+                goto out;
+        }
 
 	if (uevent)
 		dev_info(device, "firmware: requesting %s\n", name);
