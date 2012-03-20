73ede9397a01def3d5f67789d5a1f2460ce61d2f
Dorimanx <yuri@bynet.co.il>
2012-03-21 01:19:55 +0200

    Added updated lagfree and saml part for intellidemand

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -405,6 +405,9 @@ extern struct cpufreq_governor cpufreq_gov_superbad;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_intellidemand)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)	
 extern struct cpufreq_governor cpufreq_gov_lazy;	
 #define CPUFREQ_DEFAULT_GOVERNOR  	(&cpufreq_gov_lazy)	
