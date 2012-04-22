7ceb22615283126b49205bc05efd7c4bd4bcceaa
Dorimanx <yuri@bynet.co.il>
2012-04-22 03:10:21 +0300

    Switched to GCC 4.4.3 Android toolchain. Switched to optimize size to reduce warnings and broken drivers. Switched to GZIP to reduce lag with kernel compression.

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 #This script made by Dorimanx and HIEROS.
+
+COMPILER=/media/Source-Code/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+OLDCOMPILER=/root/CodeSourcery-2010.09/arm-2010.09/bin/arm-none-eabi-
+VERYOLDCOMPILER=/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi-
+
 rm -f arch/arm/boot/zImage
 rm -f /home/root/kernel/dorimanx-tytyng/output_gb/boot/zImage
 rm -f  /home/root/kernel/dorimanx-tytyng/output_gb/boot/boot.img
 rm -rf /home/root/kernel/dorimanx-tytyng/output_gb/system/lib/modules/*
 rm -rf /home/root/kernel/dorimanx-tytyng/output_gb/devs/*
-make ARCH=arm CROSS_COMPILE=/root/CodeSourcery-2010.09/arm-2010.09/bin/arm-none-eabi- zImage -j8
-#make ARCH=arm CROSS_COMPILE=/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi- zImage -j4
+make ARCH=arm CROSS_COMPILE=$COMPILER zImage -j8
 if [ -f arch/arm/boot/zImage ]; then
 if [ ! -d /home/root/kernel/dorimanx-tytyng/output_gb ]; then
 mkdir /home/root/kernel/
@@ -27,16 +31,14 @@ cp .config /home/root/kernel/dorimanx-tytyng/output_gb/devs/config
 cp .config arch/arm/configs/htcleo_defconfig
 cp arch/arm/configs/htcleo_defconfig /home/root/kernel/dorimanx-tytyng/output_gb/devs/htcleo_defconfig
 rm -rf /home/root/kernel/dorimanx-tytyng/output_gb/system/lib/modules/*
-make ARCH=arm CROSS_COMPILE=/root/CodeSourcery-2010.09/arm-2010.09/bin/arm-none-eabi- modules -j8
-make ARCH=arm CROSS_COMPILE=/root/CodeSourcery-2010.09/arm-2010.09/bin/arm-none-eabi- INSTALL_MOD_PATH=/home/root/kernel/dorimanx-tytyng/output_gb/system/lib/modules modules_install -j8
-#make ARCH=arm CROSS_COMPILE=/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi- modules
-#make ARCH=arm CROSS_COMPILE=/root/CodeSourcery/Sourcery_G++_Lite/bin/arm-none-linux-gnueabi- INSTALL_MOD_PATH=/home/root/kernel/dorimanx-tytyng/output_gb/system/lib/modules modules_install
+make ARCH=arm CROSS_COMPILE=$COMPILER modules -j8
+make ARCH=arm CROSS_COMPILE=$COMPILER INSTALL_MOD_PATH=/home/root/kernel/dorimanx-tytyng/output_gb/system/lib/modules modules_install -j8
 cd /home/root/kernel/dorimanx-tytyng/output_gb/system/lib/modules
 find -iname *.ko | xargs -i -t cp {} .
 rm -rf /home/root/kernel/dorimanx-tytyng/output_gb/system/lib/modules/lib
 stat /home/root/kernel/dorimanx-tytyng/output_gb/boot/zImage
 cd /home/root/kernel/dorimanx-tytyng/output_gb
-/root/CodeSourcery-2010.09/arm-2010.09/bin/arm-none-eabi-strip --strip-debug system/lib/modules/*.ko
+${COMPILER}strip --strip-debug system/lib/modules/*.ko
 zip -r Kernel_Dorimanx-V1.`date +"%H-%M-%d%m%y"`.zip .
 else
 echo "Kernel STUCK in BUILD! no zImage exist"
