47dae3556134d86cffcb1a3ff41465215a8ecbef
Dorimanx <yuri@bynet.co.il>
2012-05-17 13:18:18 +0300

    tproxy: added IPv6 support to the socket match
    The ICMP extraction bits were contributed by Harry Mason.
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>
    Signed-off-by: KOVACS Krisztian <hidden@balabit.hu>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    
    +Merged all net/netfilter/xt_socket.c from https://github.com/dorimanx/desire_kernel_35

--- net/netfilter/xt_socket.c
+++ net/netfilter/xt_socket.c
@@ -9,7 +9,7 @@
  * published by the Free Software Foundation.
  *
  */
-
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <linux/module.h>
 #include <linux/skbuff.h>
 #include <linux/netfilter/x_tables.h>
@@ -38,10 +38,10 @@
 void
 xt_socket_put_sk(struct sock *sk)
 {
-	if (sk->sk_state == TCP_TIME_WAIT)
-		inet_twsk_put(inet_twsk(sk));
-	else
-		sock_put(sk);
+       if (sk->sk_state == TCP_TIME_WAIT)
+               inet_twsk_put(inet_twsk(sk));
+       else
+               sock_put(sk);
 }
 EXPORT_SYMBOL(xt_socket_put_sk);
 
@@ -102,6 +102,7 @@ extract_icmp4_fields(const struct sk_buff *skb,
 	return 0;
 }
 
+
 struct sock*
 xt_socket_get4_sk(const struct sk_buff *skb, struct xt_action_param *par)
 {
@@ -143,9 +144,9 @@ xt_socket_get4_sk(const struct sk_buff *skb, struct xt_action_param *par)
 	ct = nf_ct_get(skb, &ctinfo);
 	if (ct && !nf_ct_is_untracked(ct) &&
 	    ((iph->protocol != IPPROTO_ICMP &&
-	      ctinfo == IP_CT_ESTABLISHED_REPLY) ||
+	      ctinfo == IP_CT_IS_REPLY + IP_CT_ESTABLISHED) ||
 	     (iph->protocol == IPPROTO_ICMP &&
-	      ctinfo == IP_CT_RELATED_REPLY)) &&
+	      ctinfo == IP_CT_IS_REPLY + IP_CT_RELATED)) &&
 	    (ct->status & IPS_SRC_NAT_DONE)) {
 
 		daddr = ct->tuplehash[IP_CT_DIR_ORIGINAL].tuple.src.u3.ip;
@@ -187,7 +188,7 @@ socket_match(const struct sk_buff *skb, struct xt_action_param *par,
 		if (info && info->flags & XT_SOCKET_TRANSPARENT)
 			transparent = ((sk->sk_state != TCP_TIME_WAIT &&
 					inet_sk(sk)->transparent) ||
-					(sk->sk_state == TCP_TIME_WAIT &&
+				       (sk->sk_state == TCP_TIME_WAIT &&
 					inet_twsk(sk)->tw_transparent));
 
 		xt_socket_put_sk(sk);
@@ -215,93 +216,93 @@ socket_mt4_v1(const struct sk_buff *skb, struct xt_action_param *par)
 
 static int
 extract_icmp6_fields(const struct sk_buff *skb,
-		     unsigned int outside_hdrlen,
-		     int *protocol,
-		     struct in6_addr **raddr,
-		     struct in6_addr **laddr,
-		     __be16 *rport,
-		     __be16 *lport)
+                    unsigned int outside_hdrlen,
+                    int *protocol,
+                    struct in6_addr **raddr,
+                    struct in6_addr **laddr,
+                    __be16 *rport,
+                    __be16 *lport)
 {
-	struct ipv6hdr *inside_iph, _inside_iph;
-	struct icmp6hdr *icmph, _icmph;
-	__be16 *ports, _ports[2];
-	u8 inside_nexthdr;
-	int inside_hdrlen;
+       struct ipv6hdr *inside_iph, _inside_iph;
+       struct icmp6hdr *icmph, _icmph;
+       __be16 *ports, _ports[2];
+       u8 inside_nexthdr;
+       int inside_hdrlen;
 
-	icmph = skb_header_pointer(skb, outside_hdrlen,
-				   sizeof(_icmph), &_icmph);
-	if (icmph == NULL)
-		return 1;
+       icmph = skb_header_pointer(skb, outside_hdrlen,
+                                  sizeof(_icmph), &_icmph);
+       if (icmph == NULL)
+               return 1;
 
-	if (icmph->icmp6_type & ICMPV6_INFOMSG_MASK)
-		return 1;
+       if (icmph->icmp6_type & ICMPV6_INFOMSG_MASK)
+               return 1;
 
-	inside_iph = skb_header_pointer(skb, outside_hdrlen + sizeof(_icmph), sizeof(_inside_iph), &_inside_iph);
-	if (inside_iph == NULL)
-		return 1;
-	inside_nexthdr = inside_iph->nexthdr;
+       inside_iph = skb_header_pointer(skb, outside_hdrlen + sizeof(_icmph), sizeof(_inside_iph), &_inside_iph);
+       if (inside_iph == NULL)
+               return 1;
+       inside_nexthdr = inside_iph->nexthdr;
 
-	inside_hdrlen = ipv6_skip_exthdr(skb, outside_hdrlen + sizeof(_icmph) + sizeof(_inside_iph), &inside_nexthdr);
-	if (inside_hdrlen < 0)
-		return 1; /* hjm: Packet has no/incomplete transport layer headers. */
+       inside_hdrlen = ipv6_skip_exthdr(skb, outside_hdrlen + sizeof(_icmph) + sizeof(_inside_iph), &inside_nexthdr);
+       if (inside_hdrlen < 0)
+               return 1; /* hjm: Packet has no/incomplete transport layer headers. */
 
-	if (inside_nexthdr != IPPROTO_TCP &&
-	    inside_nexthdr != IPPROTO_UDP)
-		return 1;
+       if (inside_nexthdr != IPPROTO_TCP &&
+           inside_nexthdr != IPPROTO_UDP)
+               return 1;
 
-	ports = skb_header_pointer(skb, inside_hdrlen,
-				   sizeof(_ports), &_ports);
-	if (ports == NULL)
-		return 1;
+       ports = skb_header_pointer(skb, inside_hdrlen,
+                                  sizeof(_ports), &_ports);
+       if (ports == NULL)
+               return 1;
 
-	/* the inside IP packet is the one quoted from our side, thus
-	 * its saddr is the local address */
-	*protocol = inside_nexthdr;
-	*laddr = &inside_iph->saddr;
-	*lport = ports[0];
-	*raddr = &inside_iph->daddr;
-	*rport = ports[1];
+       /* the inside IP packet is the one quoted from our side, thus
+        * its saddr is the local address */
+       *protocol = inside_nexthdr;
+       *laddr = &inside_iph->saddr;
+       *lport = ports[0];
+       *raddr = &inside_iph->daddr;
+       *rport = ports[1];
 
-	return 0;
+       return 0;
 }
 
 struct sock*
 xt_socket_get6_sk(const struct sk_buff *skb, struct xt_action_param *par)
 {
-	struct ipv6hdr *iph = ipv6_hdr(skb);
-	struct udphdr _hdr, *hp = NULL;
-	struct sock *sk;
-	struct in6_addr *daddr, *saddr;
-	__be16 dport, sport;
-	int thoff, tproto;
+       struct ipv6hdr *iph = ipv6_hdr(skb);
+       struct udphdr _hdr, *hp = NULL;
+       struct sock *sk;
+       struct in6_addr *daddr, *saddr;
+       __be16 dport, sport;
+       int thoff, tproto;
 
-	tproto = ipv6_find_hdr(skb, &thoff, -1, NULL);
-	if (tproto < 0) {
-		pr_debug("unable to find transport header in IPv6 packet, dropping\n");
-		return NF_DROP;
-	}
+       tproto = ipv6_find_hdr(skb, &thoff, -1, NULL);
+       if (tproto < 0) {
+               pr_debug("unable to find transport header in IPv6 packet, dropping\n");
+               return NF_DROP;
+       }
 
-	if (tproto == IPPROTO_UDP || tproto == IPPROTO_TCP) {
-		hp = skb_header_pointer(skb, thoff,
-					sizeof(_hdr), &_hdr);
-		if (hp == NULL)
-			return NULL;
+       if (tproto == IPPROTO_UDP || tproto == IPPROTO_TCP) {
+               hp = skb_header_pointer(skb, thoff,
+                                       sizeof(_hdr), &_hdr);
+               if (hp == NULL)
+                       return NULL;
 
-		saddr = &iph->saddr;
-		sport = hp->source;
-		daddr = &iph->daddr;
-		dport = hp->dest;
+               saddr = &iph->saddr;
+               sport = hp->source;
+               daddr = &iph->daddr;
+               dport = hp->dest;
 
-	} else if (tproto == IPPROTO_ICMPV6) {
-		if (extract_icmp6_fields(skb, thoff, &tproto, &saddr, &daddr,
-					 &sport, &dport))
-			return NULL;
-	} else {
-		return NULL;
-	}
+       } else if (tproto == IPPROTO_ICMPV6) {
+               if (extract_icmp6_fields(skb, thoff, &tproto, &saddr, &daddr,
+                                        &sport, &dport))
+                       return NULL;
+       } else {
+               return NULL;
+       }
 
-	sk = nf_tproxy_get_sock_v6(dev_net(skb->dev), tproto,
-				   saddr, daddr, sport, dport, par->in, NFT_LOOKUP_ANY);
+       sk = nf_tproxy_get_sock_v6(dev_net(skb->dev), tproto,
+                                  saddr, daddr, sport, dport, par->in, NFT_LOOKUP_ANY);
 	pr_debug("proto %hhd %pI6:%hu -> %pI6:%hu "
 		 "(orig %pI6:%hu) sock %p\n",
 		 tproto, saddr, ntohs(sport),
@@ -319,29 +320,29 @@ socket_mt6_v1(const struct sk_buff *skb, struct xt_action_param *par)
 
 	info = (struct xt_socket_mtinfo1 *) par->matchinfo;
 	sk = xt_socket_get6_sk(skb, par);
-	if (sk != NULL) {
-		bool wildcard;
-		bool transparent = true;
+       if (sk != NULL) {
+               bool wildcard;
+               bool transparent = true;
 
-		/* Ignore sockets listening on INADDR_ANY */
-		wildcard = (sk->sk_state != TCP_TIME_WAIT &&
-			    ipv6_addr_any(&inet6_sk(sk)->rcv_saddr));
+               /* Ignore sockets listening on INADDR_ANY */
+               wildcard = (sk->sk_state != TCP_TIME_WAIT &&
+                           ipv6_addr_any(&inet6_sk(sk)->rcv_saddr));
 
-		/* Ignore non-transparent sockets,
-		   if XT_SOCKET_TRANSPARENT is used */
-		if (info && info->flags & XT_SOCKET_TRANSPARENT)
-			transparent = ((sk->sk_state != TCP_TIME_WAIT &&
-					inet_sk(sk)->transparent) ||
-					(sk->sk_state == TCP_TIME_WAIT &&
-					inet_twsk(sk)->tw_transparent));
+               /* Ignore non-transparent sockets,
+                  if XT_SOCKET_TRANSPARENT is used */
+               if (info && info->flags & XT_SOCKET_TRANSPARENT)
+                       transparent = ((sk->sk_state != TCP_TIME_WAIT &&
+                                       inet_sk(sk)->transparent) ||
+                                      (sk->sk_state == TCP_TIME_WAIT &&
+                                       inet_twsk(sk)->tw_transparent));
 
-		xt_socket_put_sk(sk);
+               xt_socket_put_sk(sk);
 
-		if (wildcard || !transparent)
-			sk = NULL;
-	}
+               if (wildcard || !transparent)
+                       sk = NULL;
+       }
 
-	return (sk != NULL);
+       return (sk != NULL);
 }
 #endif
 
@@ -351,7 +352,8 @@ static struct xt_match socket_mt_reg[] __read_mostly = {
 		.revision	= 0,
 		.family		= NFPROTO_IPV4,
 		.match		= socket_mt4_v0,
-		.hooks		= 1 << NF_INET_PRE_ROUTING,
+		.hooks		= (1 << NF_INET_PRE_ROUTING) |
+				  (1 << NF_INET_LOCAL_IN),
 		.me		= THIS_MODULE,
 	},
 	{
@@ -360,19 +362,20 @@ static struct xt_match socket_mt_reg[] __read_mostly = {
 		.family		= NFPROTO_IPV4,
 		.match		= socket_mt4_v1,
 		.matchsize	= sizeof(struct xt_socket_mtinfo1),
-		.hooks		= 1 << NF_INET_PRE_ROUTING,
+		.hooks		= (1 << NF_INET_PRE_ROUTING) |
+				  (1 << NF_INET_LOCAL_IN),
 		.me		= THIS_MODULE,
 	},
 #ifdef XT_SOCKET_HAVE_IPV6
        {
-		.name           = "socket",
-		.revision       = 1,
-		.family         = NFPROTO_IPV6,
-		.match          = socket_mt6_v1,
-		.matchsize      = sizeof(struct xt_socket_mtinfo1),
-		.hooks          = (1 << NF_INET_PRE_ROUTING) |
-				  (1 << NF_INET_LOCAL_IN),
-		.me             = THIS_MODULE,
+               .name           = "socket",
+               .revision       = 1,
+               .family         = NFPROTO_IPV6,
+               .match          = socket_mt6_v1,
+               .matchsize      = sizeof(struct xt_socket_mtinfo1),
+               .hooks          = (1 << NF_INET_PRE_ROUTING) |
+                                 (1 << NF_INET_LOCAL_IN),
+               .me             = THIS_MODULE,
        },
 #endif
 };
@@ -381,7 +384,7 @@ static int __init socket_mt_init(void)
 {
 	nf_defrag_ipv4_enable();
 #ifdef XT_SOCKET_HAVE_IPV6
-	nf_defrag_ipv6_enable();
+       nf_defrag_ipv6_enable();
 #endif
 	return xt_register_matches(socket_mt_reg, ARRAY_SIZE(socket_mt_reg));
 }
