9f86d7105465f4050b92a97af3c3a88060983fb3
Dorimanx <yuri@bynet.co.il>
2012-06-04 17:46:21 +0300

    usb: gadget: adb: allow freezing in adb_read
    wait_event_interruptible in adb_read might return -ERESTARTSYS if
    userspace is frozen during adb_read or another signal is delivered
    to adb.  If so, don't set dev->error to avoid resetting the adb
    connection.
    
    Change-Id: I5a7baa013a9a3a3b5305de7e6a0d18546a560018
    Signed-off-by: Colin Cross <ccross@android.com>
    
    commit 1ebd20c6af8dd5e4d480a2616ac31afb89d52e52 1 parent a5d2e61148
    Colin Cross authored 3 months ago

--- drivers/usb/gadget/f_adb.c
+++ drivers/usb/gadget/f_adb.c
@@ -323,7 +323,8 @@ requeue_req:
 	/* wait for a request to complete */
 	ret = wait_event_interruptible(dev->read_wq, dev->rx_done);
 	if (ret < 0) {
-		dev->error = 1;
+		if (ret != -ERESTARTSYS)
+			dev->error = 1;
 		r = ret;
 		goto done;
 	}
